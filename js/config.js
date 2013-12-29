a0f3adb4a53a2e949200820a751ebeb682975a2b
David Konsumer <konsumer@jetboystudio.com>
2013-12-29 11:35:50 -0800

    lodash replaces underscore

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 $('form').submit(function(e){
 	e.preventDefault();
 	var f = $(this).serializeArray();
-	var config = _.object(f.map(function(f){ return f.name; }), f.map(function(f){ return f.value; }));
+	var config = _.zipObject(f.map(function(f){ return f.name; }), f.map(function(f){ return f.value; }));
 	console.log(config);
 	window.location.href = "pebblejs://close#" + JSON.stringify(config);
 });
