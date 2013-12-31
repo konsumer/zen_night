ff869bc35532f70ec4642ef68cbb8ff751c39bc3
David Konsumer <konsumer@jetboystudio.com>
2013-12-30 17:03:43 -0800

    persist config

--- js/config.js
+++ js/config.js
@@ -1,11 +1,14 @@
-// TODO: could I use localStorage directly? (does app js have same context?)
+// on load, grab stored values
+var config = localStorage.config ? JSON.parse(localStorage.config) : {};
+$("[name='username']").val(config.username);
+$("[name='password']").val(config.password);
+$("[name='database']").val(config.database);
+$("[name='log']").prop('checked', config.log);
 
 // on submit, zip form values & pass back to pebble js
 $('form').submit(function(e){
 	e.preventDefault();
 	var f = $(this).serializeArray();
-	var config = _.zipObject(f.map(function(f){ return f.name; }), f.map(function(f){ return f.value; }));
-	window.location.href = "pebblejs://close#" + JSON.stringify(config);
+	localStorage.config = JSON.stringify( _.zipObject(f.map(function(f){ return f.name; }), f.map(function(f){ return f.value; })) );
+	window.location.href = "pebblejs://close#" + localStorage.config;
 });
-
-console.log('perisists? ', localStorage.persists);
