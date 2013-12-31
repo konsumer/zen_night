bf0285982dff9d799569d4458858e13659732372
David Konsumer <konsumer@jetboystudio.com>
2013-12-30 16:22:45 -0800

    added database config, and testing for persistant localStorage

--- js/config.js
+++ js/config.js
@@ -1,3 +1,5 @@
+// TODO: could I use localStorage directly? (does app js have same context?)
+
 // on submit, zip form values & pass back to pebble js
 $('form').submit(function(e){
 	e.preventDefault();
@@ -5,3 +7,5 @@ $('form').submit(function(e){
 	var config = _.zipObject(f.map(function(f){ return f.name; }), f.map(function(f){ return f.value; }));
 	window.location.href = "pebblejs://close#" + JSON.stringify(config);
 });
+
+console.log('perisists? ', localStorage.persists);
